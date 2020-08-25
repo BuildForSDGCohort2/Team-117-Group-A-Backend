f2f2c9fd5ebee40f932a439b219ea4221fb62248
Dominic Chuma <chumadominic@gmail.com>
2020-08-25 18:08:44 +0100

    Created Database Tables

---
+++ To the Team_Lead/tables.sql
@@ -0,0 +1,138 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Aug 25, 2020 at 01:13 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `new`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `companies`
+--
+
+DROP TABLE IF EXISTS `companies`;
+CREATE TABLE IF NOT EXISTS `companies` (
+  `company_id` int(11) NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `address` varchar(128) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `date_registered` datetime NOT NULL,
+  PRIMARY KEY (`company_id`),
+  UNIQUE KEY `company_credentials` (`company_name`,`email`,`phone`,`address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+CREATE TABLE IF NOT EXISTS `customers` (
+  `customer_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(128) NOT NULL,
+  `last_name` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `date_registered` datetime NOT NULL,
+  PRIMARY KEY (`customer_id`),
+  UNIQUE KEY `unique_phone` (`phone`),
+  KEY `login` (`email`,`password`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `request`
+--
+
+DROP TABLE IF EXISTS `request`;
+CREATE TABLE IF NOT EXISTS `request` (
+  `request_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `test_id` int(11) NOT NULL,
+  `customer_id` int(11) UNSIGNED NOT NULL,
+  `address` text NOT NULL,
+  `request_date` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`request_id`),
+  KEY `test_type` (`test_id`),
+  KEY `customer_details` (`customer_id`),
+  KEY `request_date` (`request_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `results`
+--
+
+DROP TABLE IF EXISTS `results`;
+CREATE TABLE IF NOT EXISTS `results` (
+  `result_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) UNSIGNED NOT NULL,
+  `request_id` int(11) UNSIGNED NOT NULL,
+  `test_id` int(11) NOT NULL,
+  `result_description` text NOT NULL,
+  PRIMARY KEY (`result_id`),
+  KEY `customer_data` (`customer_id`),
+  KEY `test_type` (`test_id`),
+  KEY `request_type` (`request_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test_price`
+--
+
+DROP TABLE IF EXISTS `test_price`;
+CREATE TABLE IF NOT EXISTS `test_price` (
+  `test_id` int(11) NOT NULL AUTO_INCREMENT,
+  `test_name` varchar(128) NOT NULL,
+  `pricing` int(11) NOT NULL,
+  PRIMARY KEY (`test_id`),
+  UNIQUE KEY `test_parameters` (`test_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `request`
+--
+ALTER TABLE `request`
+  ADD CONSTRAINT `customer_constraint` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `test_contraint` FOREIGN KEY (`test_id`) REFERENCES `test_price` (`test_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `results`
+--
+ALTER TABLE `results`
+  ADD CONSTRAINT `customer_costraint` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `request_constraint` FOREIGN KEY (`request_id`) REFERENCES `request` (`request_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `test_costraint` FOREIGN KEY (`test_id`) REFERENCES `test_price` (`test_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
