3f177d22c3794b50fd0287a986889dcb94ac4cd7
Dominic Chuma <chumadominic@gmail.com>
2020-09-10 15:58:09 +0100

    Updates to the Database Schema

--- To the Team_Lead/tables.sql
+++ To the Team_Lead/tables.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Aug 25, 2020 at 01:13 PM
+-- Generation Time: Sep 10, 2020 at 02:51 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.3.5
 
@@ -30,14 +30,15 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `companies`;
 CREATE TABLE IF NOT EXISTS `companies` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(128) NOT NULL,
+  `company_id` int(11) NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(128) NOT NULL,
   `email` varchar(128) NOT NULL,
   `address` varchar(128) NOT NULL,
-  `phone` int(11) NOT NULL,
+  `phone` int(20) NOT NULL,
+  `password` varchar(200) NOT NULL,
   `date_registered` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `company_credentials` (`name`,`email`,`phone`,`address`)
+  PRIMARY KEY (`company_id`),
+  UNIQUE KEY `company_credentials` (`company_name`,`email`,`phone`,`address`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -48,14 +49,14 @@ CREATE TABLE IF NOT EXISTS `companies` (
 
 DROP TABLE IF EXISTS `customers`;
 CREATE TABLE IF NOT EXISTS `customers` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `first_name` varchar(128) NOT NULL,
   `last_name` varchar(128) NOT NULL,
   `email` varchar(128) NOT NULL,
   `password` varchar(128) NOT NULL,
   `phone` int(11) NOT NULL,
   `date_registered` datetime NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`customer_id`),
   UNIQUE KEY `unique_phone` (`phone`),
   KEY `login` (`email`,`password`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -68,15 +69,17 @@ CREATE TABLE IF NOT EXISTS `customers` (
 
 DROP TABLE IF EXISTS `request`;
 CREATE TABLE IF NOT EXISTS `request` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `request_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `test_id` int(11) NOT NULL,
   `customer_id` int(11) UNSIGNED NOT NULL,
   `address` text NOT NULL,
+  `accept` enum('0','1') NOT NULL DEFAULT '0',
   `request_date` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`request_id`),
   KEY `test_type` (`test_id`),
   KEY `customer_details` (`customer_id`),
-  KEY `request_date` (`request_date`)
+  KEY `request_date` (`request_date`),
+  KEY `accepted_requests` (`accept`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -87,31 +90,32 @@ CREATE TABLE IF NOT EXISTS `request` (
 
 DROP TABLE IF EXISTS `results`;
 CREATE TABLE IF NOT EXISTS `results` (
-  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `result_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `customer_id` int(11) UNSIGNED NOT NULL,
   `request_id` int(11) UNSIGNED NOT NULL,
   `test_id` int(11) NOT NULL,
   `result_description` text NOT NULL,
-  `result_date` timestamp NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
+  `result_date` timestamp NOT NULL,
+  PRIMARY KEY (`result_id`),
   KEY `customer_data` (`customer_id`),
   KEY `test_type` (`test_id`),
-  KEY `request_type` (`request_id`)
+  KEY `request_type` (`request_id`),
+  KEY `record_date` (`result_date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `test_price`
+-- Table structure for table `test`
 --
 
 DROP TABLE IF EXISTS `test`;
 CREATE TABLE IF NOT EXISTS `test` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(128) NOT NULL,
+  `test_id` int(11) NOT NULL AUTO_INCREMENT,
+  `test_name` varchar(128) NOT NULL,
   `pricing` int(11) NOT NULL,
   PRIMARY KEY (`test_id`),
-  UNIQUE KEY `test_parameters` (`name`)
+  UNIQUE KEY `test_parameters` (`test_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -122,16 +126,15 @@ CREATE TABLE IF NOT EXISTS `test` (
 -- Constraints for table `request`
 --
 ALTER TABLE `request`
-  ADD CONSTRAINT `customer_constraint` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `test_contraint` FOREIGN KEY (`test_id`) REFERENCES `test` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `customer_constraint` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `test_contraint` FOREIGN KEY (`test_id`) REFERENCES `test` (`test_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `results`
 --
 ALTER TABLE `results`
   ADD CONSTRAINT `customer_costraint` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `request_constraint` FOREIGN KEY (`request_id`) REFERENCES `request` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `test_costraint` FOREIGN KEY (`test_id`) REFERENCES `test` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `request_constraint` FOREIGN KEY (`request_id`) REFERENCES `request` (`request_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
